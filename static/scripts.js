ff56dc877d84e02a35e7b0be4cabfd2c5b3f39ad
Paul Zimmerman <paul@lugal.com>
2026-06-04 10:53:56 -0400

    Turn all labels red in detail view when record is voided.

--- static/scripts.js
+++ static/scripts.js
@@ -15,7 +15,16 @@ function checkForm() {
   const currentState = FormSerializer.serialize(document.getElementById('detail'));
   if (JSON.stringify(currentState) !== localStorage.getItem("initialState")) {
     document.getElementById('savebutton').disabled=false;
+
+function toggleVoided() {
+  let labels = document.querySelectorAll('label');
+  if (document.getElementById("id_voided").checked) {
+    labels.forEach(label => {
+      label.classList.add('voided');
+    });
   } else {
-    document.getElementById('savebutton').disabled=true;
+    labels.forEach(label => {
+      label.classList.remove('voided');
+    });
   };
 }
