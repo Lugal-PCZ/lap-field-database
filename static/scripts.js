d4e720b712640598734f1a594249a6e8b809e4d5
Paul C. Zimmerman <paul@lugal.com>
2026-06-18 14:30:17 -0400

    Disable new record button on detail view if there are unsaved changes.

--- static/scripts.js
+++ static/scripts.js
@@ -55,8 +55,10 @@ function checkForm() {
         };
       };
       document.getElementById('savebutton').disabled=false;
+      document.getElementById('newbutton').disabled=true;
     } else {
       document.getElementById('savebutton').disabled=true;
+      document.getElementById('newbutton').disabled=false;
     };
     if (document.querySelector("#id_voided")) {
       toggleVoided();
