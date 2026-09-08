ed78fc484e762fdb481733ab9e71defc14434906
Paul C. Zimmerman <paul@lugal.com>
2026-09-08 09:58:49 -0400

    Create lap_sampletypes table.

--- samples/models.py
+++ samples/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class SampleType(models.Model):
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        unique=True,
+        verbose_name="Type",
+    )
+
+    class Meta:
+        db_table = "lap_sampletypes"
+        verbose_name_plural = "Sample Types"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
