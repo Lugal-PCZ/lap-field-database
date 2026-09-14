e807d9244ec6ff78c5c6a22bb84d11669ad8766f
Paul C. Zimmerman <paul@lugal.com>
2026-09-14 13:54:59 -0400

    Introduced a 10ms delay so that highlightRequiredSelect2Fields() triggers more reliably when creating a blank new record form.

--- static/scripts.js
+++ static/scripts.js
@@ -1,20 +1,26 @@
-document.addEventListener(
-  'click',
-  function closePanels(event) {
-    if (event.target.parentElement.id !== "popupmenus") {
-      var thepanel = document.getElementById("loginpanel");
+window.addEventListener("load", function() {
+  cacheForm();
+  makePrevAndNextRecordLinks();
+  toggleVoided();
+  setTimeout(() => {
+    highlightRequiredSelect2Fields();
+  }, 10);
+});
+
+document.addEventListener("click", function(event) {
+  if (event.target.parentElement.id !== "popupmenus") {
+    var thepanel = document.getElementById("loginpanel");
+    if (!thepanel.hidden && !thepanel.contains(event.target)) {
+      thepanel.hidden = true;
+    };
+    if (document.querySelector("#downloadpanel")) {
+      var thepanel = document.getElementById("downloadpanel");
       if (!thepanel.hidden && !thepanel.contains(event.target)) {
         thepanel.hidden = true;
       };
-      if (document.querySelector("#downloadpanel")) {
-        var thepanel = document.getElementById("downloadpanel");
-        if (!thepanel.hidden && !thepanel.contains(event.target)) {
-          thepanel.hidden = true;
-        };
-      };
-    }
-  },
-);
+    };
+  };
+});
 
 function toggleLogin() {
   if (document.querySelector("#downloadpanel")) {
